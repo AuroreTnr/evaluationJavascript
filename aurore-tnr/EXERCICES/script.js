8f75ec7ea087edfcea4e2c8540c02cdb4eb55d0b
AuroreTnr <tournieraurore@orange.fr>
2024-09-27 11:57:20 +0200

    1 er exo

--- aurore-tnr/EXERCICES/script.js
+++ aurore-tnr/EXERCICES/script.js
@@ -1,34 +1,73 @@
 // Exercice 1 : calcul du nombre de jeunes, de moyens et de vieux
-let values = 0;
 
-function filterAge() {
+// function filterAge() {
 
-    values = parseInt(prompt("Entrez les ages :"));
-
-    let inferieur20 = 0;
-    let superieur40 = 1;
-    let entre20Et40 = 0;
+//     values = parseInt(prompt("Entrez les ages :"));
+    
 
 
-    if (values < 20) {
-        inferieur20 + 1;
-    }
-    else if (values < 40) {
-        superieur40 + 1;
-    }
-    else{
-        entre20Et40 + 1;
-    }
+//     let countInferieur20 = 0;
+//     let countSuperieur40 = 0;
+//     let countEntre20Et40 = 0;
 
-   console.log(inferieur20);
+
+//     if (values < 20) {
+//         countInferieur20++;
+//     }
+//     else if (values < 40) {
+//         countSuperieur40++;
+//     }
+//     else{
+//         countEntre20Et40++;
+//     }
+
+//     console.log(countInferieur20);
    
     
     
-}
+// }
 
-do {
+// do {
 
-    filterAge();
+//     filterAge();
     
-} while (values != 100);
+// } while (values != 100);
+
+
+
+// let count = 0;
+
+// function filterAge(count) {
+
+    
+//     values = parseInt(prompt("Entrez les ages :"));
+    
+
+
+//     if (values < 20) {
+//         count++
+//     }
+
+    
+    
+// }
+
+
+// do {
+    
+//     filterAge()
+    
+// } while (values != 100);
+
+// console.log(count);
+
+
+
+let count;
+count++
+
+console.log(count);
+
+
+
 
