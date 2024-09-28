8c055b79d7514ef7457033a471455934d1a42b39
Aurore <tournieraurore@orange.fr>
2024-09-28 17:58:59 +0200

    style fait + mise en page + premier exo resolu

--- aurore-tnr/EXERCICES/script.js
+++ aurore-tnr/EXERCICES/script.js
@@ -1,73 +1,66 @@
 // Exercice 1 : calcul du nombre de jeunes, de moyens et de vieux
 
-// function filterAge() {
+const btnExo1 = document.querySelector("#exo-1");
+const reponse1 = document.querySelector(".reponse1");
+btnExo1.addEventListener("click", filterAge);
 
-//     values = parseInt(prompt("Entrez les ages :"));
-    
-
-
-//     let countInferieur20 = 0;
-//     let countSuperieur40 = 0;
-//     let countEntre20Et40 = 0;
+function filterAge() {
+    //RESET REPONSE
+    reponse1.textContent="";
 
 
-//     if (values < 20) {
-//         countInferieur20++;
-//     }
-//     else if (values < 40) {
-//         countSuperieur40++;
-//     }
-//     else{
-//         countEntre20Et40++;
-//     }
-
-//     console.log(countInferieur20);
-   
-    
-    
-// }
-
-// do {
-
-//     filterAge();
-    
-// } while (values != 100);
+    let countInferieur20 = 0;
+    let countSuperieur40 = 0;
+    let countEntre20Et40 = 0;
 
 
+    do {
 
-// let count = 0;
+        values = parseInt(prompt("Entrez les ages :"));
+        
+        
+        if (values < 20) {
+            countInferieur20++;
+        }
+        else if (values > 40) {
+            countSuperieur40++;
+        }
+        else{
+            countEntre20Et40++;
+        }
 
-// function filterAge(count) {
+    } while (values != 100);
 
-    
-//     values = parseInt(prompt("Entrez les ages :"));
-    
+    reponse1.textContent= `Réponse : Il y a ${countInferieur20} personne(s) dont l' age est inférieur à 20 ans; ${countSuperieur40} personne(s) dont l' age est superieur à 40 ans et ${countEntre20Et40} personne(s) qui ont entre 20 et 40 ans`;
 
+    return reponse1;
+};
 
-//     if (values < 20) {
-//         count++
-//     }
-
-    
-    
-// }
+// Exercice 2 : Table de multiplication
+const btnExo2 = document.querySelector("#exo-2");
+const reponse2 = document.querySelector(".reponse2");
+btnExo2.addEventListener("click", tableMultiplication);
 
 
-// do {
-    
-//     filterAge()
-    
-// } while (values != 100);
+// Exercice 3 : recherche d'un prénom
+const btnExo3 = document.querySelector("#exo-3");
+const reponse3 = document.querySelector(".reponse3");
+btnExo3.addEventListener("click", searchPrenom);
 
-// console.log(count);
+
+// Exercice 4 : total d'une commande
+const btnExo4 = document.querySelector("#exo-4");
+const reponse4 = document.querySelector(".reponse4");
+btnExo4.addEventListener("click", prixPap);
+
+
+// Exercice 5 : vérification d'un formulaire (lien sur l' index)
 
 
 
-let count;
-count++
-
-console.log(count);
-
+// BUTTON FLOAT
+const btnFloat = document.querySelector(".btn-float");
+btnFloat.addEventListener("click", scrollTop);
 
 
 
